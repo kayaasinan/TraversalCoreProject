0176d014ce86604f1e229ffdc130ff9b7350da21
Sinan Kaya <skaya.sinan@outlook.com>
2025-10-01 20:48:44 +0300

    added login and register templates

---
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
