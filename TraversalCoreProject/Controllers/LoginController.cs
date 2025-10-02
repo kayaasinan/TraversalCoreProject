6ffd40f5e530d32d130e63084ddc3d1439c856a7
Sinan Kaya <skaya.sinan@outlook.com>
2025-10-02 18:34:54 +0300

    created member/user layout partial and login operations

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Scaffolding.Shared.Project;
+using System.Threading.Tasks;
 using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
-    public class LoginController(UserManager<AppUser> _userManager) : Controller
+    public class LoginController(UserManager<AppUser> _userManager,
+                                    SignInManager<AppUser> _signInManager) : Controller
     {
         public IActionResult SignUp()
         {
@@ -29,13 +31,13 @@ namespace TraversalCoreProject.Controllers
                 Email = model.Mail,
                 UserName = model.UserName
             };
-            if (model.Password==model.ComfirmPassword)
+            if (model.Password == model.ComfirmPassword)
             {
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded) return RedirectToAction("SignIn");
                 else
                 {
-                    foreach(var item in result.Errors)
+                    foreach (var item in result.Errors)
                     {
                         ModelState.AddModelError("", item.Description);
                     }
@@ -46,6 +48,28 @@ namespace TraversalCoreProject.Controllers
         public IActionResult SignIn()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByNameAsync(model.userName);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Bu e-mail sistemde kayıtlı değil");
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, model.password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Email veya şifre hatalı");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Destination");
         }
     }
 }
---
+++ TraversalCoreProject/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını giriniz")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string password { get; set; }
+    }
+}
