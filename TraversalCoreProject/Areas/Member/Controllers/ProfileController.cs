d4d902e86b861d42619948fac7a69268f7ccaa46
Sinan Kaya <skaya.sinan@outlook.com>
2025-10-03 14:12:27 +0300

    added profile update (includes image upload)

--- TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProject/Areas/Member/Controllers/ProfileController.cs
@@ -7,6 +7,7 @@ using TraversalCoreProject.Areas.Member.Models;
 namespace TraversalCoreProject.Areas.Member.Controllers
 {
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class ProfileController(UserManager<AppUser> _userManager) : Controller
     {
         public async Task<IActionResult> Index()
@@ -14,9 +15,47 @@ namespace TraversalCoreProject.Areas.Member.Controllers
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             var model = new UserEditViewModel();
             model.name = user.Name;
-            model.surname= user.SurName;
+            model.surname = user.SurName;
             model.phoneNumber = user.PhoneNumber;
             model.mail = user.Email;
+            model.imageUrl = user.ImageUrl;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (model.Image != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(model.Image.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/userImages/" + imageName;
+                using (var stream = new FileStream(saveLocation, FileMode.Create))
+                {
+                    await model.Image.CopyToAsync(stream);
+                }
+                user.ImageUrl = imageName;
+
+            }
+            user.Name = model.name;
+            user.SurName = model.surname;
+            if (!string.IsNullOrEmpty(model.password))
+            {
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, model.password);
+            }
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.password))
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Profile", new { area = "Member" });
+                }
+            }
             return View(model);
         }
     }
