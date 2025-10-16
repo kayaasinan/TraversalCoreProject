5b8d03fe4fc36812069b29107d384ab725b94c14
Sinan Kaya <kayaasinan@gmail.com>
2025-10-16 13:49:11 +0300

    _

---
+++ TraversalCoreProject/ViewComponents/HelperComponent/_MemberLayoutEmptyPageComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.HelperComponent
+{
+    public class _MemberLayoutEmptyPageComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var emptyPageMessage = "Görüntülenecek rezervasyon bulunamadı";
+            return View("Default", emptyPageMessage);
+        }
+    }
+}
