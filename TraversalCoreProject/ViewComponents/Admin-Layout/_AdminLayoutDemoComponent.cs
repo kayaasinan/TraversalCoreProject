72641bc253dc52ba632059c4b0c03740f872e5ad
Sinan Kaya <kayaasinan@gmail.com>
2025-10-16 21:39:26 +0300

    added admin layout component

---
+++ TraversalCoreProject/ViewComponents/Admin-Layout/_AdminLayoutDemoComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Admin_Layout
+{
+    public class _AdminLayoutDemoComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Admin-Layout/_AdminLayoutSideBarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Admin_Layout
+{
+    public class _AdminLayoutSideBarComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
