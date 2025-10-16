c92799f49192ffbb2d58ef32b81da4e5551d89ca
Sinan Kaya <kayaasinan@gmail.com>
2025-10-16 16:00:07 +0300

    added member dashboard component

---
+++ TraversalCoreProject/Areas/Member/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DashboardController(UserManager<AppUser> _userManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userName = values.Name + " " + values.SurName;
+            ViewBag.images = values.ImageUrl;
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation(UserManager<AppUser> _userManager) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.fullName = values.Name + " " + values.SurName;
+            ViewBag.email = values.Email;
+            ViewBag.phoneNumber = values.PhoneNumber;
+            return View();
+        }
+    }
+}
