7ce2738d4d2f2fc77414ac389956340a10490166
Sinan Kaya <kayaasinan@gmail.com>
2025-10-17 18:22:26 +0300

    added admin dashboard component

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_AdminDashboardHead.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _AdminDashboardHead : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_Cards1Statistic.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistic : ViewComponent
+    {
+        private readonly Context _context;
+
+        public _Cards1Statistic(Context context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = _context.Destinations.Count();
+            ViewBag.v2 = _context.Users.Count();
+            return View();
+        }
+    }
+}
