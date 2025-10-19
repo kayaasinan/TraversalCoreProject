d35ea340f1b90279c73804b807940d3f20b31d83
Sinan Kaya <kayaasinan@gmail.com>
2025-10-19 17:22:45 +0300

    added pdf interface and class

---
+++ BusinessLayer/Concrete/PdfManager.cs
@@ -0,0 +1,74 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLayer.Concrete
+{
+    public class PdfManager : IPdfService
+    {
+        private readonly Context _context;
+
+        public PdfManager(Context context)
+        {
+            _context = context;
+        }
+        public void CreateDynamicReservationReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/reservation_report.pdf");
+            using var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            using var document = new Document(PageSize.A4);
+
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+
+       
+            PdfPTable table = new PdfPTable(4);
+            table.WidthPercentage = 100;
+
+            table.AddCell("Ad Soyad");
+            table.AddCell("Rota");
+            table.AddCell("Kişi Sayısı");
+            table.AddCell("Rezervasyon Tarihi");
+
+           
+            var reservations = _context.Reservations
+                .Include(x => x.AppUser)
+                .Include(x => x.Destination)
+                .Select(x => new
+                {
+                    FullName = x.AppUser.Name + " " + x.AppUser.SurName,
+                    Destination = x.Destination.City,
+                    PeopleCount = x.NumberOfPeople,
+                    Date = x.ReservationDate
+                })
+                .ToList();
+
+       
+            foreach (var item in reservations)
+            {
+                table.AddCell(item.FullName);
+                table.AddCell(item.Destination);
+                table.AddCell(item.PeopleCount.ToString());
+                table.AddCell(item.Date.ToString("dd.MM.yyyy"));
+            }
+
+            document.Add(table);
+            document.Close();
+        }
+
+        public void CreateStaticPdfReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/file1.pdf");
+            using var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+
+            using var document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+
+            document.Add(new Paragraph("Traversal Rezervasyon Statik Pdf Raporu"));
+            document.Close();
+        }
+    }
+}
--- TraversalCoreProject/Controllers/PDFReportController.cs
+++ TraversalCoreProject/Controllers/PDFReportController.cs
@@ -1,4 +1,5 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Mvc;
@@ -7,71 +8,23 @@ using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
-    public class PDFReportController(Context _context) : Controller
+    public class PDFReportController(IPdfService _pdfService) : Controller
     {
+      
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult StaticPdfReport()
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PDFReports/file1.pdf");
-            using var stream = new FileStream(path, FileMode.Create);
-
-            using var document = new Document(PageSize.A4);
-            PdfWriter.GetInstance(document, stream);
-
-            document.Open();
-
-            Paragraph paragraph = new Paragraph("Traversal Rezervasyon Statik Pdf Raporu");
-
-            document.Add(paragraph);
-
-            document.Close();
-
+            _pdfService.CreateStaticPdfReport();
             return File("/PDFReports/file1.pdf", "application/pdf", "file1.pdf");
         }
-        public List<ReservationModel> ReservationList()
-        {
-            return _context.Reservations.Include(x => x.AppUser).Include(x => x.Destination).Select(x => new ReservationModel
-            {
-                Name = x.AppUser.Name + " " + x.AppUser.SurName,
-                Destination = x.Destination.City,
-                PeopleCount = x.NumberOfPeople,
-                ReservationDate = x.ReservationDate
-            }).ToList();
-        }
+    
         public IActionResult DynamicReservationReport()
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pdfreports/reservation_report.pdf");
-
-            using (var stream = new FileStream(path, FileMode.Create))
-            using (var document = new Document(PageSize.A4))
-            {
-                PdfWriter.GetInstance(document, stream);
-                document.Open();
-
-                PdfPTable table = new PdfPTable(4);
-                table.WidthPercentage = 100;
-
-                table.AddCell("Ad Soyad");
-                table.AddCell("Rota");
-                table.AddCell("Kişi Sayısı");
-                table.AddCell("Rezervasyon Tarihi");
-
-                var reservations = ReservationList();
-                foreach (var item in reservations)
-                {
-                    table.AddCell(item.Name);
-                    table.AddCell(item.Destination);
-                    table.AddCell(item.PeopleCount.ToString());
-                    table.AddCell(item.ReservationDate.ToString("dd.MM.yyyy"));
-                }
-
-                document.Add(table);
-            }
-
-            return File("/pdfreports/reservation_report.pdf", "application/pdf", "RezervasyonRaporu.pdf");
+            _pdfService.CreateDynamicReservationReport();
+            return File("/PDFReports/reservation_report.pdf", "application/pdf", "RezervasyonRaporu.pdf");
         }
     }
 }
