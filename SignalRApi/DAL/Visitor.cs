33d8ff68d0b645e0d1e546711be6270c34c2ca79
Sinan Kaya <kayaasinan@gmail.com>
2025-10-23 19:57:49 +0300

    added new project(WEBAPI(singalR))

---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,18 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum ECity
+    {
+        Manisa=1,
+        İstanbul=2,
+        Çankırı=3,
+        Gaziantep=4,
+        Antalya=5
+    }
+    public class Visitor
+    {
+        public int VisitorId { get; set; }
+        public ECity City { get; set; }
+        public int DailyVisitorCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
